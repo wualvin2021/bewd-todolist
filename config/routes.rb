bbbbffa2ed6b2b9caea7d576c26c7499b07b9d9c
Alvin Wu <wualvin2021@gmail.com>
2024-11-07 14:23:16 -1000

    config restored

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'homepage#index'
+
+  # Add routes below this line
+
+  # Add routes below above line
+
+  # Redirect all other paths to index page, which will be taken over by AngularJS
+  get '*path' => 'homepage#index'
+end
